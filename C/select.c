e93d7eb04eb90daaf8b557e7817ebb5bdddda0c0
zhanjang <wzh20070605@163.com>
2025-09-21 18:14:17 +0800

    c语言学习

--- C/select.c
+++ C/select.c
@@ -1,9 +1,16 @@
+//分支结构
 #include <stdio.h>
-//选择语句
 int main()
 {
     int num1 = 0;
-    scanf("%d",num1);
+    scanf("%d",&num1);
+    int num2 = 0;
+    scanf("%d",&num2);
+    ifelse(num1);
+    whilecontrl(num2);
+}
+int ifelse(int num1)
+{
     if (num1 > 0)
     {
         printf("非零\n");
@@ -12,10 +19,11 @@ int main()
     {
         printf("零或负数\n");
     }
-//循环语句
-    int num2 = 0;
-    scanf("%d",num2);
-    while (num2 > 0)
+
+}
+int whilecontrl(int num2)
+{
+        while (num2 > 0)
     {
         printf("num2=%d\n", num2);
         num2--;
