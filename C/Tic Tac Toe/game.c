dea638e36c22353c0c0dd6aef06305f88778391a
zhanjang <wzh20070605@163.com>
2025-11-07 22:50:24 +0800

    c语言学习

--- C/Tic Tac Toe/game.c
+++ C/Tic Tac Toe/game.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "ifgame.h"
+char chess[3][3];
 void reset_board(char n[][3])//重置棋盘函数
 {
     int i = 0,j = 0;
@@ -11,64 +12,45 @@ void reset_board(char n[][3])//重置棋盘函数
         } 
     }
 }
+int play(int player,char a)
+{
+    int n1 = 0,m1 = 0,clear = 0,n2 = 0,m2 = 0;
+    for (n1 = 0;n1< 3;n1++)
+    {
+        for (m1 = 0; m1 < 3;m1++)
+        {
+            printf("%c ",chess[n1][m1]);
+        }
+        printf("\n");
+    }
+    while ((clear = getchar()) != '\n' && clear != EOF);//清理缓存区
+    printf("输入你要下的位置");
+    scanf("(%d,%d)",&n2,&m2);
+    if (chess[n2-1][m2-1] == ' ')
+    {
+        chess[n2-1][m2-1] = a;
+        ifgame(chess);
+        return !player;
+    }
+    else
+    {
+        printf("该位置已被占用!\n");
+        return player;
+    }
+}
 int main()
 {
-    int player = 1;
-    char chess[3][3];
+    int player = 0;
     reset_board(chess);
     while (id)//游戏主体
     {
-        if(player == 1)
+        if(player == 0)
         {
-            int n = 0,m = 0,clear = 0;
-            for (n = 0;n< 3;n++)
-            {
-                for (m = 0; m < 3;m++)
-                {
-                    printf("%c ",chess[n][m]);
-                }
-                printf("\n");
-            }
-            while ((clear = getchar()) != '\n' && clear != EOF);//清理缓存区
-            printf("输入你要下的位置");
-            scanf("(%d,%d)",&n,&m);
-            if (chess[n-1][m-1] == ' ')
-            {
-                chess[n-1][m-1] = 'X';
-                ifgame(chess);
-                player = 2;
-            }
-            else
-            {
-                printf("该位置已被占用!\n");
-                player = 1;
-            }
+            player = play(player,'X');
         }
-        else if(player == 2)
+        else if(player == 1)
         {
-            int n = 0,m = 0,clear = 0;
-            for (n = 0;n< 3;n++)
-            {
-                for (m = 0; m < 3;m++)
-                {
-                    printf("%c ",chess[n][m]);
-                }
-                printf("\n");
-            }
-            while ((clear = getchar()) != '\n' && clear != EOF);
-            printf("输入你要下的位置");
-            scanf("(%d,%d)",&n,&m);
-            if (chess[n-1][m-1] == ' ')
-            {
-                chess[n-1][m-1] = 'O';
-                ifgame(chess);
-                player = 1;
-            }
-            else
-            {
-                printf("该位置已被占用!\n");
-                player = 2;
-            }          
+            player = play(player,'O');
         }
     }
 }
