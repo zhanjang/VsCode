7da698df341534594c5147c5cbbe500df280cf74
zhanjang <wzh20070605@163.com>
2025-11-12 23:28:21 +0800

    c语言学习

--- C/myfunc/Linear List/Sequential list/seqlist.c
+++ C/myfunc/Linear List/Sequential list/seqlist.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "seqlist.h"
-Slist* initialize(int element_count)
+Slist* slist_creat(int element_count)
 {
     Slist *list = malloc(sizeof(Slist));
-    if (list == NULL)
+    if (list == NULL)//检查list是否创建成功
     {
-        printf("not enough memory for list structre");
-        exit(0);
+        printf("not enough memory for list structre\n");
+        return NULL;
     }
-    
     list->capacity = element_count;
     list->num = 0;
     list->data = malloc(element_count*sizeof(Seqtype));
-    if (list->data == NULL) {
-        printf("not enough memory for data");
-        free(list);
+    if (list->data == NULL)//检查data是否创建成功
+    {
+        printf("not enough memory for data\n");
+        free(list);//如果失败则释放list的内存并返回NULL
         return NULL;
     }
     return list;
 }
-void del_list(Slist* list)
+void slist_destroy(Slist* list)
 {
     if (list == NULL)
     {
-        printf("pointer is null");
+        printf("pointer is null\n");
         return;
     }
     list->capacity = 0;
@@ -33,25 +33,87 @@ void del_list(Slist* list)
     list->data = NULL;
     free(list);
 }
-void clear_all_data(Slist *list)
+void slist_clear(Slist *list)
 {
     if (list == NULL)
     {
-        printf("pointer is null");
+        printf("pointer is null\n");
         return;
     }
     list->num = 0;
 }
-size_t size(Slist *list)
+size_t slist_size(Slist *list)
 {
     if (list == NULL)
     {
-        printf("pointer is null");
+        printf("pointer is null\n");
         return 0;
     }
     return list->num;
 }
-Seqtype get(Slist *list,int id)
+Seqtype slist_get(Slist *list,int id)
 {
-
+    if (list != NULL && list->data != NULL && id >= 0 && id < list->num)
+    {
+        return list->data[id];
+    }
+    exit(0);
+}
+void slist_set(Slist *list,int id,Seqtype d)
+{
+    if (list != NULL && list->data != NULL && id >= 0 && id < list->num)
+    {
+        list->data[id] = d;
+    }
+}
+void slist_insert(Slist *list,int id,Seqtype d)
+{
+    Seqtype *temp = list->data;
+    if (list != NULL && list->data != NULL && id >= 0 && id < list->num)
+        if (list -> capacity < list-> num + 1)
+        {
+            list->capacity *= 2;
+            temp = malloc(sizeof(Seqtype)*(list->capacity));
+            if (temp == NULL)
+            {
+                printf("not enough memory\n");
+                exit(0);
+            }
+            for (size_t i = 0; i < list->num; i++) 
+            {
+            temp[i] = list->data[i];
+            }
+            free(list->data);
+            list->data = temp;
+        }
+        for (size_t i = list->num; i > id; i--) 
+        {
+        list->data[i] = list->data[i - 1];
+        }
+        list->data[id] = d;
+        list->num += 1;
+}
+Seqtype slist_remove(Slist *list,int id)
+{
+    if (list != NULL && list->data != NULL && id >= 0 && id < list->num)
+    {
+        Seqtype res = list->data[id];
+        for (size_t i = id + 1;i < list->num;i++)
+        {
+            list->data[i-1] = list->data[i];
+        }
+        list->num -= 1;
+        return  res;
+    }
+}
+int slist_index(Slist *list,Seqtype key)
+{
+    for (size_t i = 0;i < list->num;i++)
+    {
+        if (list->data[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
--- C/myfunc/Linear List/Sequential list/seqlist.h
+++ C/myfunc/Linear List/Sequential list/seqlist.h
@@ -8,9 +8,13 @@ struct Seqlist
     size_t capacity;//线性表能存储的元素个数
     Seqtype *data;//线性表的首元素地址
 };
-Slist* initialize(int element_count);//创建一个线性表,并返回线性表的地址,element_count表示元素个数
-void del_list(Slist* list);//删除一个线性表,list表示线性表的地址
-void clear_all_data(Slist *list);//将线性表的元素个数清空
-size_t size(Slist *list);//查询线性表的元素个数
-Seqtype get(Slist *list,int id);//获取线性表中第i个元素
+Slist* slist_creat(int element_count);//创建一个线性表,并返回线性表的地址,element_count表示元素个数
+void slist_destroy(Slist* list);//删除一个线性表,list表示线性表的地址
+void slist_clear(Slist *list);//将线性表的元素个数清空
+size_t slist_size(Slist *list);//查询线性表的元素个数
+Seqtype slist_get(Slist *list,int id);//获取线性表中第i个元素
+void slist_set(Slist *list,int id,Seqtype d);//将第id位置的元素设置为d
+void slist_insert(Slist *list,int id,Seqtype d);//在id位置插入元素d
+Seqtype slist_remove(Slist *list,int id);//删除id位置的元素,并返回这个元素值
+int slist_index(Slist *list,Seqtype key);//返回值为key的位置
 #endif
